3a3106db2021697a27ca5040e4894c3b337dd357
Danny Blackstock <dannyblackstock@gmail.com>
2017-03-19 05:37:50 -0700

    writes to danny’s “Database” of how many trips he has

--- public/index.js
+++ public/index.js
@@ -1,17 +1,41 @@
-$('#press-me').click(function() {
-  $.get( '/getUserName', function(data) {
-    console.log(data);
-    //  $('body').html(data.UserName);
-     data.Data.forEach( function (arrayItem)
-      {
-          var x = arrayItem.FuelEfficiency.Value;
-          $('body').append('<li>'+x+'</li>');
+
+$('document').ready(function() {
+
+  // get all danny's trips
+  $.get( '/getDanny', function(data) {
+      console.log(data);
+      data.Data.forEach( function (arrayItem) {
+        var x = arrayItem.FuelEfficiency.Value;
+        $('body').append('<li>'+x+'</li>');
       });
    });
-});
-$('#press-josh').click(function() {
-  $.get( '/getUser2', function(data) {
-    console.log(data);
-     $('body').html(data.UserName);
+   // get danny's civic government stats
+   $.get('http://www.fueleconomy.gov/ws/rest/vehicle/17554', function(data) {
+     $xml = $(data);
+     $combined = $xml.find("comb08").text();
+     console.log($combined);
+     $('body').append('<p>dannys civic combined mpg from govt is: '+$combined+'</p>');
+   }, 'xml');
+
+   // get danny's hardcoded data
+   $.get('/getDannyStats', function(data) {
+     var obj = JSON.parse(data);
+     console.log(obj);
    });
+
+   // get all josh's trips
+   $.get( '/getUser2', function(data) {
+     console.log(data);
+      $('body').html(data.UserName);
+  });
+
+  // get josh's jeep government stats
+  $.get('http://www.fueleconomy.gov/ws/rest/vehicle/30784', function(data) {
+   //  var parsed = $.parseXML(data);
+    $xml = $(data);
+   //  console.log(data);
+    $combined = $xml.find("comb08").text();
+    console.log($combined);
+    $('body').append('<p>joshs jeep combined mpg from govt is: '+$combined+'</p>');
+  }, 'xml');
 });

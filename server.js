b85d5712d071c6e885a5c43043c5fbef2fc09896
Danny Blackstock <dannyblackstock@gmail.com>
2017-03-19 10:44:21 -0700

    refactored backend

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var express = require('express');
 var fs = require('fs')
 var MojioClientLite= require("mojio-client-lite");
 var app = express();
+var request = require('request');
+var parser = require('xml2json');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -43,21 +45,93 @@ app.get('/getDanny', function (req, res) {
       // login successful
       // write your logic here
       console.log('success');
+      // For each user
       mojio_client.getPath('/v2/trips/').then(function(result, error) {
-          console.log(result);
+          // 1. get all mojio trips
+          // 2. get all government info
+
+          // For each mojio trip fuel efficiency val, compare to the government value
+
+          // return all the users with their points and trips
+          // console.log(result);
           data = result;
-          // data = result;
-          var m = JSON.parse(fs.readFileSync('public/database.json').toString());
-          m[0].NumberOfTrips = result.Data.length;
-          fs.writeFile('public/database.json', JSON.stringify(m));
-          res.send(result);
+          var databaseInfo = JSON.parse(fs.readFileSync('public/database.json').toString());
+          databaseInfo[0].NumberOfTrips = result.Data.length;
+          fs.writeFile('public/database.json', JSON.stringify(databaseInfo));
+
+          // get danny's civic government stats
+          request('http://www.fueleconomy.gov/ws/rest/vehicle/' + databaseInfo[0].VehicleId, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              // console.log(body) // Print the body of response.
+              // xml to json
+              var json = parser.toJson(body);
+              json = JSON.parse(json)
+              console.log('json:' + json); // Print the body of response.
+
+              var govtMpg = json.vehicle.comb08;
+              // console.log('govt combined mpg:' + govtMpg); // Print the body of response.
+              var dannyPoints = 0;
+              result.Data.forEach(function(element) {
+                console.log(element);
+                var difference = (element.FuelEfficiency.Value*2.352146) - govtMpg;
+                if (difference > 0) {
+                  // add the points together
+                  // write to the backend
+                  console.log('govt combined mpg:' + govtMpg)
+                  console.log((element.FuelEfficiency.Value*2.352146) + 'mpg, great trip!');
+                  dannyPoints += difference;
+                } else {
+                  console.log('govt combined mpg:' + govtMpg)
+                  console.log((element.FuelEfficiency.Value*2.352146) + 'mpg, bad/ok trip!');
+                }
+              });
+
+              var databaseInfo = JSON.parse(fs.readFileSync('public/database.json').toString());
+              databaseInfo[0].Points = dannyPoints;
+              // add the database info to the return request
+              data.databaseInfo = databaseInfo[0];
+              fs.writeFile('public/database.json', JSON.stringify(databaseInfo));
+              res.send(data);
+            }
+          });
       });
   })
 
   // res.send(data);
 });
+// 
+// app.get('/getUser2', function (req, res) {
+//   var config = {
+//       application: 'c1616a16-9cd1-4b8a-9201-5fd6dcfe68a2',
+//       secret:'34ce1d9d-6251-4652-8539-5ffc5be3d0d7'
+//   };
+//
+//   var mojio_client = new MojioClientLite(config);
+//   var data;
+//
+//   // No authorized user, redirect to Mojio authentication server.
+//   mojio_client.authorize('joshcheng','ssbg123').then(function(response,error1){
+//
+//       if(typeof(err)!="undefined")
+//       {
+//           console.log("login error");
+//           return;
+//       }
+//
+//       // login successful
+//       // write your logic here
+//       console.log('success');
+//       mojio_client.getPath('/v2/trips/{94cd834d-57eb-48df-b0fc-d7584e8cfb11}').then(function(result, error) {
+//           // console.log(result);
+//           res.send(result);
+//       });
+//   })
+//
+//   // res.send(data);
+// });
 
-app.get('/getUser2', function (req, res) {
+// use res.render to load up an ejs view file
+app.get('/getJosh', function (req, res) {
   var config = {
       application: 'c1616a16-9cd1-4b8a-9201-5fd6dcfe68a2',
       secret:'34ce1d9d-6251-4652-8539-5ffc5be3d0d7'
@@ -78,23 +152,59 @@ app.get('/getUser2', function (req, res) {
       // login successful
       // write your logic here
       console.log('success');
-      mojio_client.getPath('/v2/trips/{94cd834d-57eb-48df-b0fc-d7584e8cfb11}').then(function(result, error) {
-          console.log(result);
-          res.send(result);
+      // For each user
+      mojio_client.getPath('/v2/trips/').then(function(result, error) {
+          // 1. get all mojio trips
+          // 2. get all government info
+
+          // For each mojio trip fuel efficiency val, compare to the government value
+
+          // return all the users with their points and trips
+          // console.log(result);
+          data = result;
+          var databaseInfo = JSON.parse(fs.readFileSync('public/database.json').toString());
+          databaseInfo[1].NumberOfTrips = result.Data.length;
+          fs.writeFile('public/database.json', JSON.stringify(databaseInfo));
+
+          // get danny's civic government stats
+          request('http://www.fueleconomy.gov/ws/rest/vehicle/' + databaseInfo[1].VehicleId, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              // console.log(body) // Print the body of response.
+              // xml to json
+              var json = parser.toJson(body);
+              json = JSON.parse(json)
+              console.log('json:' + json); // Print the body of response.
+
+              var govtMpg = json.vehicle.comb08;
+              // console.log('govt combined mpg:' + govtMpg); // Print the body of response.
+              var joshPoints = 0;
+              result.Data.forEach(function(element) {
+                console.log(element);
+                var difference = (element.FuelEfficiency.Value*2.352146) - govtMpg;
+                if (difference > 0) {
+                  // add the points together
+                  // write to the backend
+                  console.log('govt combined mpg:' + govtMpg)
+                  console.log((element.FuelEfficiency.Value*2.352146) + 'mpg, great trip!');
+                  joshPoints += difference;
+                } else {
+                  console.log('govt combined mpg:' + govtMpg)
+                  console.log((element.FuelEfficiency.Value*2.352146) + 'mpg, bad/ok trip!');
+                }
+              });
+
+              var databaseInfo = JSON.parse(fs.readFileSync('public/database.json').toString());
+              databaseInfo[1].Points = joshPoints;
+              // add the database info to the return request
+              data.databaseInfo = databaseInfo[1];
+              fs.writeFile('public/database.json', JSON.stringify(databaseInfo));
+              res.send(data);
+            }
+          });
       });
   })
 
   // res.send(data);
-});
-
-app.get('/getDannyStats', function(req, res) {
-  fs.readFile('public/database.json', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    res.send(data);
-    console.log(data);
-  });
 });
 
 app.listen(3000, function () {

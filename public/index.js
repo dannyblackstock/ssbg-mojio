b0369af4a1f877e8edc5771b0831725f73b68f30
jncheng <jncheng@sfu.ca>
2017-03-19 09:04:12 -0700

    Updated Front end Visuals
    lots of stuff

--- public/index.js
+++ public/index.js
@@ -1,14 +1,47 @@
-$('#press-me').click(function() {
+$(document).ready(function(){
   $.get( '/getUserName', function(data) {
-    console.log(data);
+    var lastTripDur = data.Data;
+    var lastTripVal = lastTripDur[lastTripDur.length - 1].Duration;
+    // lastTripVal = Math.round(lastTripVal *10)/10;
+    // console.log(lastTripVal);
+    lastTripVal = lastTripVal.substring(0, lastTripVal.indexOf('.'));
+    // console.log(lastTripVal);
+
+
+    var lastTripMPG = data.Data/* some array here */;
+    var last_element = lastTripMPG[lastTripMPG.length - 1];
+
+    var lastTripLength = data.Data;
+    var lastTripKM = lastTripLength[lastTripLength.length - 1].Distance.Value;
+
+
+    // var totalDriven = data.Data;
+    // for (var i = totalDriven.length - 1; i >= 0; i--) {
+    //   totalDriven = totalDriven[i] + totalDriven[i+1];
+    // }
+    // console.log(totalDriven);
+    // // var totalDrivenTime = totalDriven[totalDriven].Duration.reduce(add, 0);
+
+
+    //   console.log(data);
     //  $('body').html(data.UserName);
-     data.Data.forEach( function (arrayItem)
-      {
-          var x = arrayItem.FuelEfficiency.Value;
-          $('body').append('<li>'+x+'</li>');
-      });
-   });
+     // data.Data.forEach( function (arrayItem)
+     //  {
+          
+     //  });
+        // var x = arrayItem.FuelEfficiency.Value;
+        // $('#fuelEcoTrips').append();
+// var x = arrayItem.FuelEfficiency.Value;
+    $('#fuelEcoTrips').append(last_element.FuelEfficiency.Value);
+    $('#ltLength').append(lastTripVal);
+    $('#ltDistance').append(lastTripKM/1000 );
+
+
+  });
+
+
 });
+
 $('#press-josh').click(function() {
   $.get( '/getUser2', function(data) {
     console.log(data);
